6c84aa10ba7d2fe150e427e73b71f80e446ce386
Aashna <bajwa.aashna@gmail.com>
2019-06-15 21:37:37 -0400

    Stopped at removeStudent Classroom

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,42 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Classroom {
+
+    private Student[] students;
+    List<Student> studentList = new ArrayList<>(Arrays.asList(students));
+
+    public Classroom(int maxNumberOfStudents){
+        this.students = new Student[maxNumberOfStudents];
+    }
+
+    public Classroom(Student[] students){
+        this.students = students;
+    }
+
+    public Classroom(){
+        this.students = new Student[30];
+    }
+
+    public Student[] getStudents(){
+        return students;
+    }
+
+    public double getAverageExamScore(){
+        double sum = 0.0;
+        for(Student student : students){
+            sum += student.getAverageExamScore();
+        }
+        return sum / students.length;
+    }
+
+    public void addStudent(Student student){
+        studentList.add(student);
+        students = studentList.toArray(new Student[0]);
+    }
+    
 }
+
